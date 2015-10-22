c5590598be8531cf0dd45b8ceca5f00a99da02bd
radekstasiak <radek.stasiak@gmail.com>
2015-10-22 22:47:51 +0100

    Update get_json.py
    ziomeczku, 6 linijka tam gdzie wolasz url to nie powienies uzywac DEMO_KEY,
    
    public static final String NASA_API_KEY="7DHPawbYZdAk5NHgIeA7EwtslTeWEdxrQUsNcwUU";
    
    tą cytowaną zawartość sobie przeklej i najlepiej stwórz taką stałą w odzielnym pliku, 
    całego urla tak sobie rozbij, jak np. ja mam 
    
    public class Constants {
        public static final String NASA_API_URL="https://api.nasa.gov/planetary";
        public static final String NASA_API_FORMAT="JSON";
        public static final String NASA_API_KEY="7DHPawbYZdAk5NHgIeA7EwtslTeWEdxrQUsNcwUU";
        public static final int NASA_API_RESPONSE_NUMBERY=10;
        public static final String STORAGE_FILENAME = "apod_list";
        public static final String NASA_API_APOD="/apod?api_key=%1$s&date=%2$s&format="+NASA_API_FORMAT;
        public static final String PREF_NAME = "NASAPref";
    }
    
    wszystko jest rozbite i potem łatwiej jest zmieniać, elo

--- request/get_json.py
+++ request/get_json.py
@@ -1,8 +1,12 @@
 from urllib.request import urlopen
 from .models import Data
 
+    """
+    komentarz sobie przeczytaj na dole, elo
+    """
 
 def present_json_to_db():
+
     url = 'https://api.nasa.gov/planetary/apod?concept_tags=True&date=2015-10-22&hd=True&api_key=DEMO_KEY'
     content = urlopen(url).read().decode("utf-8")
     d = eval(content)
