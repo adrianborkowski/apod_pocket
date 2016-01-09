31c00df7f9a8d61702478a7ab360f72713e56b67
adrianborkowski <adi@netten.pl>
2016-01-09 14:31:52 +0100

    change database settings v3.2

--- apod_pocket/settings.py
+++ apod_pocket/settings.py
@@ -120,14 +120,3 @@ STATICFILES_DIRS = (  # for heroku
     os.path.join(BASE_DIR, 'static'),
 )
 
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
-
