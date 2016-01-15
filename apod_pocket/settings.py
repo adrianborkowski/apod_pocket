80a681f05cdae97e7a2cdba45152e1132b44071b
adrianborkowski <adi@netten.pl>
2016-01-15 22:40:11 +0100

    add rest api on heroku

--- apod_pocket/settings.py
+++ apod_pocket/settings.py
@@ -83,11 +83,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'apod_pocket',
-        'USER': 'admin',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = 'staticfiles'
 
-# DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
