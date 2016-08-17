a5a36e5e82be74617525b19069b191905aa28a05
adrianborkowski <adbor88@gmail.com>
2016-08-17 22:00:25 +0200

    Improve archive and correct RWD of main page./2

--- apod_pocket/settings.py
+++ apod_pocket/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-# DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
