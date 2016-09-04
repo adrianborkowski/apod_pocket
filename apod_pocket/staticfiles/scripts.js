3de0ce79f135889ddf515698e444148ce2ba570d
adrianborkowski <adbor88@gmail.com>
2016-09-04 18:56:37 +0200

    Add parallax effect

---
+++ apod_pocket/staticfiles/scripts.js
@@ -0,0 +1,57 @@
+// animated scroll
+$(document).ready(function(){
+  $('body').scrollspy({target: ".navbar", offset: 50});
+  $("#myNavbar a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+// navbar auto height offset
+$(document).ready(function(){
+    $(".navbar").affix({offset: {top: $("#header").outerHeight(true)} });
+});
+
+// parallaxing
+var images = [].slice.call(document.querySelectorAll('.parallax'));
+function getViewportHeight() {
+    var a = document.documentElement.clientHeight, b = window.innerHeight;
+    return a < b ? b : a;
+}
+
+function getViewportScroll() {
+    if(typeof window.scrollY != 'undefined') {
+        return window.scrollY;
+    }
+    if(typeof pageYOffset != 'undefined') {
+        return pageYOffset;
+    }
+    var doc = document.documentElement;
+    doc = doc.clientHeight ? doc : document.body;
+    return doc.scrollTop;
+}
+
+function doParallax() {
+    var el, elOffset, elHeight,
+        offset = getViewportScroll(),
+        vHeight = getViewportHeight();
+
+    for(var i in images) {
+        el = images[i];
+        elOffset = el.offsetTop;
+        elHeight = el.offsetHeight;
+
+        if((elOffset > offset + vHeight) || (elOffset + elHeight < offset)) { continue; }
+
+        el.style.backgroundPosition = '50% '+Math.round((elOffset - offset)*3/8)+'px';
+    }
+}
+
+Gator(window).on('scroll', doParallax);
--- request/update.py
+++ request/update.py
@@ -8,8 +8,7 @@ from apod_pocket.settings import URL
 
 def new():
     """
-    This function check in every 15 minutes if newest apod is available
-    and download it to database if it does not exists.
+    This function download newest apods to database and then check in every 15 minutes if newest apod is available.
     """
     current_date = date.today()
     latest_date = Data.objects.dates('date', 'day').order_by('-date')[0]
@@ -23,7 +22,7 @@ def new():
                 d = eval(request.urlopen(URL.format(date=day)).read().decode("utf-8"))
             except error.HTTPError as err:
                 print(datetime.now(), ': {0}'.format(err))
-                Timer(900, new).start()
+                break
             Data.objects.create(title=d.get('title'),
                                 date=d.get('date'),
                                 url=d.get('url'),
@@ -42,7 +41,7 @@ def new():
 
 def old():
     """
-    This function allows dowload older apods by typing date and period.
+    This function allows to dowload older apods by typing date and period.
     """
     print('APPENDING DATA TO DATABASE')
     date_entry = input('Enter a start date in YYYY-MM-DD format: ')
